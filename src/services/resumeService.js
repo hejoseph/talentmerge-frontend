7d97c0dcc4d0f3a1a51e1a7857b8436da7ebab8f
joseph <josephhe255@gmail.com>
2025-11-28 23:13:17 +0100

    trying to integrate resume service

---
+++ src/services/resumeService.js
@@ -0,0 +1,18 @@
+import api from './api';
+
+const uploadResume = (file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return api.post('/resumes/upload', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    });
+};
+
+const resumeService = {
+    uploadResume,
+};
+
+export default resumeService;
